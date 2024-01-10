c9f05c5194a867e2f7d2fe9c4a8fc6296bae10d2
Finley Edwards <92788657+Fin0201@users.noreply.github.com>
2024-01-10 15:43:29 +0000

    Fixed session start issue

--- member.php
+++ member.php
@@ -1,14 +1,12 @@
 <?php
+    $title = 'Member Page';
+    require __DIR__ . "/inc/header.php";
     require_once 'inc/functions.php';
-    session_start();
     
     if (!isset($_SESSION['user']))
     {
         redirect('login', ["error" => "You need to be logged in to view this page"]);
     }
-
-    $title = 'Member Page';
-    require __DIR__ . "/inc/header.php";
 ?>
 
 <h1>Welcome <?= $_SESSION['user']['firstname'] ?? 'Member' ?>!</h1>
